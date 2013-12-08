baa1f05d372f653fa994dd9f83d3f88715c229f1
parahunter <parahunter@gmail.com>
2013-12-08 13:31:28 +0000

    work on day 2 example

--- Assets/GameController/DisplayBulletCount.cs
+++ Assets/GameController/DisplayBulletCount.cs
@@ -6,12 +6,14 @@ public class DisplayBulletCount : MonoBehaviour
 	
 	public Rect countPosition; //where should the score be placed?
 	
-	public TankShoot shootScript; //reference to the script with the count
-		
+	public TankShoot shootScript; //reference to the script with the bullet count
+	public TankHealth healthScript; //reference to the script with the health count
+	
 	//when we want to draw GUI it happens inside this function
 	void OnGUI()
 	{
 		//draw some text
-		GUI.Label(countPosition, "bullets: " + shootScript.bulletCount);	
+		GUI.Label(countPosition, "bullets: " + shootScript.bulletCount + " \nhealth: " + healthScript.health);
+		
 	}
 }
---
+++ Assets/Tank/TankHealth.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankHealth : MonoBehaviour 
+{
+	
+	
+	public float startHealth = 100f;
+	
+	public float health
+	{
+		get;
+		private set;
+	}
+	
+	// Use this for initialization
+	void Start () 
+	{
+		health = startHealth;
+	}
+	
+	public void GiveDamage(float damage)
+	{
+		health -= damage;
+		
+		if(health < 0)
+			Destroy(gameObject);
+	}
+}
