0de700cfd578e03080b548670e125f325b39e797
parahunter <parahunter@gmail.com>
2014-06-12 23:38:57 +0100

    started to add editor examples

---
+++ Assets/EditorExamples/GizmoExample/CubeController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CubeController : MonoBehaviour 
+{
+	
+	GizmoExample[] cubeMovers;
+	
+	Vector3 targetPos;
+	
+	void Awake()
+	{
+		cubeMovers = (GizmoExample[])GameObject.FindObjectsOfType<GizmoExample>();
+	}
+	
+	void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.white;
+		Gizmos.DrawWireSphere(targetPos, 1f);
+		
+		
+	}
+	
+	void Update()
+	{
+		Ray screenRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+		
+		RaycastHit hit;
+		if(Physics.Raycast(screenRay, out hit))
+		{
+			targetPos = hit.point;
+		}
+		
+		foreach(GizmoExample cubeMover in cubeMovers)
+		{
+			cubeMover.direction = (targetPos - cubeMover.transform.position).normalized;
+		}
+		
+	}
+	
+	
+}
---
+++ Assets/EditorExamples/GizmoExample/GizmoExample.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GizmoExample : MonoBehaviour 
+{
+	public float force = 400f;
+	
+	public Vector3 direction
+	{
+		get;
+		set;
+	}
+	
+	void OnDrawGizmos()
+	{
+		Gizmos.DrawLine(transform.position, transform.position + direction * 3);
+		
+		Gizmos.color = Color.red;
+		Gizmos.DrawLine(transform.position, transform.position + rigidbody.velocity);
+	}
+	
+	void FixedUpdate()
+	{
+		rigidbody.AddForce(direction * force);	
+	}
+	
+}
