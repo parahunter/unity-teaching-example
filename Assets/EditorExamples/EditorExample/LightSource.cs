f38972cdc06609a211504337e427b2cceb557c18
parahunter <parahunter@gmail.com>
2014-06-17 18:12:22 +0100

    started editor example

---
+++ Assets/EditorExamples/EditorExample/LightSource.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class LightSource 
+{
+	public Light light;
+	public float duration;
+	
+	public float maxIntensity
+	{
+		get;
+		private set;
+	}
+	
+	public void Init()
+	{
+		maxIntensity = light.intensity;
+	}
+	
+}
